46ce9ea497ed423f31735ce7f7b10e1ef247cd1b
Asus <miguelangelhoyosv@gmail.com>
2022-05-06 16:56:38 -0500

    Upload#17

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,111 @@
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(const eda());
+void main() {
+  runApp(MiApp());
+}
 
-class eda extends StatelessWidget{
-  const eda ({Key? key}) : super (key: key);
+class MiApp extends StatelessWidget {
+  const MiApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "EDA",
       home: Inicio(),
-
     );
   }
-
-
 }
 
 class Inicio extends StatefulWidget {
-  Inicio({Key? key}) : super (key: key);
+  Inicio({Key? key}) : super(key: key);
 
   @override
   _InicioState createState() => _InicioState();
 }
 
-class _InicioState extends State <Inicio>{
+class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: const Center (
-        child: Image(
-          image: AssetImage("../Img/EDA2.png"),
-          width: 100,
-          height: 100,
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: const Center (
+            child: Image(
+              image: AssetImage("../Img/EDA2.png"),
+              width: 100,
+              height: 100,
             ),
+          ),
         ),
-      ),
-      body: Container(
-        child: _campos(),
-
-      )
-
-
+        backgroundColor: Color(0xFFFEF2),
+        body: Container(
+            child: cuerpo()
+        )
     );
   }
 }
 
 Widget cuerpo(){
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.start,
-    mainAxisSize: MainAxisSize.max,
-    children: <Widget>[
-      Container(
-        child: const Center(
-            child: Text("Formulario de Registro"),
-        ),
-      )
-    ],
+  return Container(
+    child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            textos(),
+            SizedBox(height: 50),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  botonIniciarSesion(),
+                  SizedBox(width: 60),
+                  botonRegistrarse()
+                ]
+            )
+          ],
+        )
+    ),
   );
 }
 
-class _campos extends StatelessWidget {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                )
-              ),
-            ),
-          ],
-        )
-    );;
-  }
+Widget botonIniciarSesion(){
+  return FlatButton(
+      color: Color(0xff9d00d1),
+      padding: EdgeInsets.symmetric(horizontal: 55, vertical: 25),
+      onPressed: (){}, child: Text("Iniciar sesión", style: TextStyle(fontSize: 20, color: Colors.white),),
+      shape: RoundedRectangleBorder(side: BorderSide(color: Color(0xFFFEF2)), borderRadius: BorderRadius.circular(30.0))
+  );
 }
 
+Widget botonRegistrarse(){
+  return FlatButton(
+    color: Color(0xFFFEF2),
+    padding: EdgeInsets.symmetric(horizontal: 55, vertical: 25),
+    onPressed: (){}, child: Text("Registrarse", style: TextStyle(fontSize: 20, color: Color(0xff9d00d1)),),
+    shape: RoundedRectangleBorder(side: BorderSide(color: Color(0xff9d00d1)), borderRadius: BorderRadius.circular(30.0)),
+  );
+}
 
+Widget textos(){
+  return Container(
+      width: 700,
+      height: 350,
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xff9d00d1), width: 3),
+        borderRadius: BorderRadius.circular(40.0),
+      ),
+      child: text()
+  );
+}
 
-
-
-
-
-
-
+Widget text(){
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text("Bienvenido a EDA", style: TextStyle(color: Colors.black, fontSize: 45.0, fontWeight: FontWeight.bold)),
+      SizedBox(
+        height: 25,
+      ),
+      Text("¡Estamos muy entusiasmados con \n   tu visita! Solo basta un clic para \n   comenzar a construir una mejor \n             versión de ti mismo. \n\n        ¿Por dónde empezamos?", style: TextStyle(color: Colors.black, fontSize: 30.0),),
+    ],
+  );
+}
